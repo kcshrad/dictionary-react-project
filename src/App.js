584a375e62b3110228f757b03521098af668a527
kcshrad <kcshrad@gmail.com>
2022-03-12 22:21:58 +1100

    search engine created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./images.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Shraddha Thapa</footer>
+      </div>
     </div>
   );
 }
