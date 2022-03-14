41940df0e019a29476c2db6bf36a95fe31ba5240
kcshrad <kcshrad@gmail.com>
2022-03-14 21:27:58 +1100

    heading added to the page

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ export default function App() {
       <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h3>let's find your word</h3>
         </header>
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer">Coded by Shraddha Thapa</footer>
+        <footer className="App-footer">
+          <a href="https://github.com/kcshrad/dictionary-react-project">
+            Open source code
+          </a>
+          , by Shraddha Thapa{" "}
+        </footer>
       </div>
     </div>
   );
