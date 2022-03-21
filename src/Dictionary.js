6175775115b08166ba64d8d553f370ba10988982
kcshrad <kcshrad@gmail.com>
2022-03-21 23:26:33 +1100

    integrated photoapi to upload photos of searched word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+  function handlePexelResponse(response) {
+    //console.log(response.data);
+    setPhotos(response.data.photos);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -21,6 +27,11 @@ export default function Dictionary(props) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     //console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
+    let pexelsApiKey =
+      "563492ad6f91700001000001aa1a3b94e9114f1eadc93af563af1588";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -39,6 +50,7 @@ export default function Dictionary(props) {
           <div className="hint">Hint: yoga, morning , happy, goal...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
